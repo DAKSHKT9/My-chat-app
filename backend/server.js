31c0bfc9b743373dc67bb92ba7e8c7cac6ee2017
DAKSHKT9 <dakshkatkar89@gmail.com>
2024-07-16 19:19:21 +0530

    added mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { chats } = require("./data/data");
 const dotenv = require("dotenv");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running good");
@@ -23,4 +26,4 @@ app.get("/api/chat/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server started on port ${PORT}`));
+app.listen(PORT, console.log(`server started on port ${PORT}`.red.bold));
